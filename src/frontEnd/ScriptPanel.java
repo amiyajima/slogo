08dbb63896e9ba52fa09560dca202768de166429
elichtenberg93 <erl15@duke.edu>
2014-10-04 14:14:02 -0400

    TextField changed to TextArea to allow multiple line input

--- src/frontEnd/ScriptPanel.java
+++ src/frontEnd/ScriptPanel.java
@@ -1,8 +1,8 @@
 package frontEnd;
 
-import javafx.geometry.Pos;
+//import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -13,16 +13,16 @@ class ScriptPanel extends Pane {
 	ScriptPanel(BorderPane borderPane, Controller controller) {
 		HBox hbox = new HBox();
 		
-		TextField textField = new TextField("Enter commands here");
+		TextArea textArea = new TextArea("Enter commands here");
 		Button runButton = new Button("RUN");
-		textField.setPrefSize(3*borderPane.getPrefWidth()/4, borderPane.getPrefHeight()/5);
-		textField.setAlignment(Pos.TOP_LEFT);
+		textArea.setPrefSize(3*borderPane.getPrefWidth()/4, borderPane.getPrefHeight()/5);
+//		textArea.setAlignment(Pos.TOP_LEFT);
 		runButton.setPrefSize(borderPane.getPrefWidth()/4, borderPane.getPrefHeight()/5);
 		
-		runButton.setOnAction(event -> controller.runScript(textField.getText()));
+		runButton.setOnAction(event -> controller.runScript(textArea.getText()));
 //		textField.setOnAction(event -> controller.runScript(textField.getText()));
 		
-		hbox.getChildren().addAll(textField, runButton);
+		hbox.getChildren().addAll(textArea, runButton);
 		
 		borderPane.setBottom(hbox);
 	}
