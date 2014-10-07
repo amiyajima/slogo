3243f97965ba1e89c744e27b0c6da8d3295711ec
Ethan Chang <ejc24@duke.edu>
2014-10-07 00:29:51 -0400

    changed operationcommand class

--- src/commands/OperationCommand.java
+++ src/commands/OperationCommand.java
@@ -1,15 +1,9 @@
 package commands;
 
-import java.util.List;
-
 public abstract class OperationCommand extends Command {
 
-    private List<Command> myChildren;
-
-    public abstract Double execute();
-    
-    protected List<Command> getMyChildren() {
-        return myChildren;
+    public OperationCommand() {
+        super();
     }
     
 }
