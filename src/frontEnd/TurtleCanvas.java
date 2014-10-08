c5d58144e1044c930c3eb09ab83e6dc4f8886885
Brian Bolze <brianbolze@gmail.com>
2014-10-08 01:19:13 -0400

    Lots of changes to TurtleCanvas stuff

--- src/frontEnd/TurtleCanvas.java
+++ src/frontEnd/TurtleCanvas.java
@@ -1,20 +1,55 @@
 package frontEnd;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import backEnd.Controller;
 
-public class TurtleCanvas extends Canvas {
+public class TurtleCanvas extends Group {
+	
+	private double myWidth;
+	private double myHeight;
+	Rectangle myBackground;
+	ImageView turtleView;
 
 	public TurtleCanvas(double width, double height, Controller controller) {
-		super(3.*width/4., 4.*height/5.);
+		super();
+		
+		myWidth = 3.*width/4.;
+		myHeight = 4.*height/5.;
+		
+		addBackground();
+		addTurtle();
+	}
+	
+	public void setTurtleX(double x) {
+		turtleView.setX(x - turtleView.getImage().getWidth()/2);
+	}
+	
+	public void setTurtleY(double y) {
+		turtleView.setY(y - turtleView.getImage().getWidth()/2);
+	}
+	
+	public void changeBackgroundColor(Color c) {
+		myBackground.setFill(c);
 	}
 
-	public void changeBackgroundColor(Color c) {
-		GraphicsContext gc = getGraphicsContext2D();
-		gc.setFill(c);
-		gc.fillRect(0, 0, getWidth(), getHeight());
+	private void addBackground() {
+		myBackground = new Rectangle();
+		myBackground.setWidth(myWidth);
+		myBackground.setHeight(myHeight);
+		myBackground.setFill(Color.WHITE);
+		myBackground.setStroke(Color.BLACK);
+		getChildren().add(myBackground);
+	}
+	
+	private void addTurtle() {
+		turtleView = new ImageView(new Image(getClass().getResourceAsStream("../resources/images/rcd.png")));
+		turtleView.setX(myWidth/2 - turtleView.getImage().getWidth()/2);
+		turtleView.setY(myHeight/2 - turtleView.getImage().getHeight()/2);
+		getChildren().add(turtleView);
 	}
 
 }
---
+++ src/titlePanes/HistoryTitlePane.java
@@ -0,0 +1,11 @@
+package titlePanes;
+
+import javafx.scene.control.TitledPane;
+
+class HistoryTitlePane extends TitledPane {
+
+	public HistoryTitlePane() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
