afe61e7195ad19232aaa0c562a3a1efc9b300f39
Brian Bolze <brianbolze@gmail.com>
2014-10-09 13:03:17 -0400

    Added drawables

---
+++ src/drawer/Drawable.java
@@ -0,0 +1,12 @@
+package drawer;
+
+import java.util.List;
+
+import javafx.scene.Node;
+
+public abstract class Drawable extends Node {
+
+	public Drawable(List<Object> params) {
+	}
+
+}
---
+++ src/drawer/Line.java
@@ -0,0 +1,38 @@
+package drawer;
+
+import java.util.List;
+
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+
+class Line extends Drawable {
+
+	public Line(List<Object> params) {
+		super(params);
+	}
+
+	@Override
+	protected NGNode impl_createPeer() {
+		return null;
+	}
+
+	@Override
+	public BaseBounds impl_computeGeomBounds(BaseBounds bounds, BaseTransform tx) {
+		return null;
+	}
+
+	@Override
+	protected boolean impl_computeContains(double localX, double localY) {
+		return false;
+	}
+
+	@Override
+	public Object impl_processMXNode(MXNodeAlgorithm alg,
+			MXNodeAlgorithmContext ctx) {
+		return null;
+	}
+
+}
