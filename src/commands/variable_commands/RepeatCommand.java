a9cc08ff56b9b76ad940ae5ab5654c0c94a64100
Anna Miyajima <am437@duke.edu>
2014-10-09 14:17:23 -0400

    repeat command implemented

--- src/commands/variable_commands/RepeatCommand.java
+++ src/commands/variable_commands/RepeatCommand.java
@@ -3,24 +3,32 @@ package commands.variable_commands;
 import backEnd.Model;
 import commands.Command;
 
+
 public class RepeatCommand extends Command {
+
+    public RepeatCommand () {
+        super();
+        setNumChildren(2);
+    }
 
     @Override
     public Double execute () {
-        // TODO Auto-generated method stub
-        return null;
+        Double result = 0.0;
+        for (int i = 0; i < getMyChildren().get(0).execute(); i++) {
+            result += getMyChildren().get(1).execute();
+        }
+        return result;
     }
 
     @Override
     public String toString () {
-        // TODO Auto-generated method stub
-        return null;
+        return "repeat" + getMyChildren().get(1).execute() + " " + getMyChildren().get(0).execute() + " times." ;
     }
 
     @Override
     public void initializeCommand (Model m) {
         // TODO Auto-generated method stub
-
+        
     }
 
 }
