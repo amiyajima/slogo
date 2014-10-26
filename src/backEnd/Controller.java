a8b122c9c87afc35f1b2c172e4ecfb914268ab1f
Brian Bolze <brianbolze@gmail.com>
2014-10-26 03:53:55 -0400

    Checkstyle stuff

--- src/backEnd/Controller.java
+++ src/backEnd/Controller.java
@@ -12,94 +12,98 @@ import main.ResourceFinder;
 import frontEnd.View;
 
 /**
- * 
  * @author Brian Bolze
  *
  */
 public class Controller {
-	
+
+	/**
+	 * Used by Workspace to attach the appropriate MouseEvent EventHandler to
+	 * the stage
+	 */
 	public EventHandler<MouseEvent> myMouseListener;
+
 	public EventHandler<KeyEvent> myKeyListener;
-	
+
 	private Model myModel;
 	private View myView;
 
-	public Controller (Model model, View view) {
+	public Controller(Model model, View view) {
 		myModel = model;
 		myView = view;
-		
+
 		myView.addControllerAndSetupGui(this);
 		myModel.setupTurtleManager(view);
-		
+
 		buildMouseListener();
 		buildKeyListener();
 
-		myView.bindToModelProperties(myModel.getBackgroundIndex(), myModel.getPaletteArguments());
+		myView.bindToModelProperties(myModel.getBackgroundIndex(),
+				myModel.getPaletteArguments());
 	}
 
-	public void runScript (String script) {
+	public void runScript(String script) {
 		if (script != null) {
 			try {
-			    myModel.runScript(script);
-			}
-			catch (Exception e) {
+				myModel.runScript(script);
+			} catch (Exception e) {
 				myView.printException(e);
 			}
 			myView.addToHistory(script);
 		}
 	}
-	
+
 	/**
 	 * Called by keys and buttons
 	 * 
-	 * @param The base command that is being executed
+	 * @param The
+	 *            base command that is being executed
 	 */
-	public void move10 (String direction) {
+	public void move10(String direction) {
 		ResourceBundle commands = ResourceFinder.getMyLanguageResources();
 		String temp = commands.getString(direction);
 		String[] command = temp.split(",");
-		runScript(command[0] +" 10");
+		runScript(command[0] + " 10");
 	}
 
-	public void changeBackgroundColor (Color c) {
+	public void changeBackgroundColor(Color c) {
 		myView.changeBackgroundColor(c);
 	}
 
-	public void changePenColor (Color c) {
+	public void changePenColor(Color c) {
 		myView.changePenColor(c);
 	}
 
-	public void changeTurtleImage (File f) {
+	public void changeTurtleImage(File f) {
 		myView.changeTurtleImage(f);
 	}
 
-	public void toggleGridLines () {
+	public void toggleGridLines() {
 		myView.toggleGridLines();
 	}
-	
-	public void setVariableProperties(File f){
-	    myModel.setVariableProperties(f);
+
+	public void setVariableProperties(File f) {
+		myModel.setVariableProperties(f);
 	}
-	
+
 	private void buildMouseListener() {
 		myMouseListener = new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-//				double x = event.getX();
-//				double y = event.getY();
-				
+				// double x = event.getX();
+				// double y = event.getY();
 
 			}
 		};
 	}
-	
+
 	private void buildKeyListener() {
 		myKeyListener = new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent keyEvent) {
-				
+
 				KeyCode key = keyEvent.getCode();
-				
+
 				try {
 					if (key.equals(KeyCode.W)) {
 						move10("Forward");
--- src/panels/ScriptPanel.java
+++ src/panels/ScriptPanel.java
@@ -7,33 +7,24 @@ import javafx.scene.layout.Pane;
 import backEnd.Controller;
 
 public class ScriptPanel extends Pane {
-	
 	private TextArea myTextArea;
 
 	public ScriptPanel(double width, double height, Controller controller) {
-		
 		HBox hbox = new HBox();
 		hbox.setMinWidth(width);
 		hbox.setMinHeight(height);
-		
 		myTextArea = new TextArea();
 		myTextArea.setPromptText("Enter commands here");
 		Button runButton = new Button("RUN");
-		
 		runButton.setOnAction(event -> handler(controller, myTextArea));
-				
 		myTextArea.setPrefSize(3.*width/4., height);
 		runButton.setPrefSize(width/4., height);
-		
 		hbox.getChildren().addAll(myTextArea, runButton);
 		getChildren().add(hbox);
-
 	}
-	
 	public void printException(Exception e) {
 		myTextArea.setText(e.toString());
 	}
-	
 	private void handler(Controller c, TextArea t) {
 		try {
 			String text = t.getText();
