761d7b53bdc64956cc117f663e83ba5e77b834f9
elichtenberg93 <erl15@duke.edu>
2014-10-31 05:33:52 -0400

    VariableTitlePane slightly refactored

--- src/titlePanes/VariableTitlePane.java
+++ src/titlePanes/VariableTitlePane.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Eli Lichtenberg
+
 package titlePanes;
 
 import java.io.IOException;
@@ -12,63 +15,81 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import backEnd.Controller;
 
+/**
+ * TitledPane responsible for displaying variables to user. Display is done
+ * within GUI's side panel
+ * 
+ * @author Brian Bolze
+ * @author Ethan Chang
+ * @author Eli Lichtenberg
+ * @author Anna Miyajima
+ *
+ */
 public class VariableTitlePane extends TitledPane {
 	
+	private static final String TITLE = "Variables";
+	private static final String VAR_PROPS_FILE_NAME = "/resources/Variables.properties";
 	private VBox myRoot;
-	@SuppressWarnings("unused")
 	private Properties myVarProps;
 	private Map<String, String> myVarMap;
 	
-	public VariableTitlePane(Controller contr) throws IOException {
-		setText("Variables");
+	/**
+	 * Constructor for VariableTitlePane. Takes in the Workspace's Controller 
+	 * to allow for further interaction with user to be implemented
+	 * 
+	 * @param contr Controller of workspace
+	 * @throws IOException
+	 */
+	public VariableTitlePane (Controller contr) throws IOException {
+		setText(TITLE);
 		myRoot = new VBox();
 		setContent(myRoot);
-		
-		myVarProps = new Properties();
-		InputStream fileInput = getClass().getResourceAsStream("/resources/Variables.properties");
-		myVarProps.load(fileInput);
+		myVarProps = makeNewProperties(VAR_PROPS_FILE_NAME);
 		myVarMap = new HashMap<String, String>();
 	}
-
-	public void updateVariables() throws IOException {
-		myVarProps = new Properties();
-		InputStream fileInput = getClass().getResourceAsStream("/resources/Variables.properties");
-		myVarProps.load(fileInput);
+	
+	private Properties makeNewProperties (String resourceFileName) throws IOException {
+		Properties props = new Properties();
+		InputStream fileInput = getClass().getResourceAsStream(resourceFileName);
+		props.load(fileInput);
+		return props;
+	}
+	
+	/**
+	 * Checks if there is a difference between variables properties file 
+	 * and a map containing the most recently updated variables. If there is 
+	 * a difference, the list of variables is rewritten in the TitledPane 
+	 * in the GUI.
+	 * 
+	 * @throws IOException
+	 */
+	public void updateVariables () throws IOException {
+		myVarProps = makeNewProperties(VAR_PROPS_FILE_NAME);
 		Set<Object> keys = myVarProps.keySet();
-		for(Object o : keys) {
-			String key = (String)o;
-			String val = myVarProps.getProperty(key);
-			if(!myVarMap.containsKey(key) || !myVarMap.get(key).equals(val) 
-					|| myVarMap.keySet().size()!=keys.size()) {
-				writeVariables(keys);
-				break;
+		if (myVarMap.keySet().size() != keys.size()) {
+			writeVariablesToScreen(keys);
+		}
+		else {
+			for (Object o : keys) {
+				String key = (String)o;
+				String val = myVarProps.getProperty(key);
+				if (!myVarMap.get(key).equals(val)) {
+					writeVariablesToScreen(keys);
+					break;
+				}
 			}
 		}
 	}
 
-	private void writeVariables(Set<Object> keys) {
+	private void writeVariablesToScreen (Set<Object> keys) {
 		myRoot.getChildren().clear();
 		myVarMap.clear();
-		for(Object o : keys) {
+		for (Object o : keys) {
 			String key = (String)o;
 			String val = myVarProps.getProperty(key);
-			Label variable = new Label(key + " = " + val);
-			myRoot.getChildren().add(variable);
+			Label varLabel = new Label(key + " = " + val);
+			myRoot.getChildren().add(varLabel);
 			myVarMap.put(key, val);
 		}
 	}
-	
-	
-	
-//	public void setupVariableMap(Map<String, Double> varMap) {
-//		Set<String> variables = varMap.keySet();
-//		for(String varName : variables) {
-//			Label variable = new Label(varName + ": " + varMap.get(varName));
-//			myRoot.getChildren().add(variable);
-//		}
-//	}
-	
-//	private void addListeners() {
-//		varMap.addL
-//	}
 }
